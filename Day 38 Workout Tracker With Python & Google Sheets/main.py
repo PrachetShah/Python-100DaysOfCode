5b479363d7004a89ea896f8acecfc436ed879a25
PrachetShah <75517374+PrachetShah@users.noreply.github.com>
2021-07-03 16:16:13 +0530

    Add files via upload

---
+++ Day 38 Workout Tracker With Python & Google Sheets/main.py
@@ -0,0 +1,54 @@
+import requests
+from datetime import datetime
+import os
+
+GENDER = YOUR GENDER
+WEIGHT_KG = YOUR WEIGHT
+HEIGHT_CM = YOUR HEIGHT
+AGE = YOUR AGE
+
+APP_ID = os.environ["NT_APP_ID"]
+API_KEY = os.environ["NT_API_KEY"]
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+sheet_endpoint = os.environ["SHEET_ENDPOINT"]
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+
+today = datetime.now().strftime("%d/%m/%Y")
+now = datetime.now().strftime("%X")
+
+bearer_headers = {
+    "Authorization": f"Bearer {os.environ['TOKEN']}"
+}
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today,
+            "time": now,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs, headers=bearer_headers)
+
+    print(sheet_response.text)
