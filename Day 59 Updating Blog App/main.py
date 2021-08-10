9c89acf67a00e271e5597a48bcbce9b8a9ebd12f
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2021-08-10 16:34:55 +0530

    Add files via upload

---
+++ Day 59 Updating Blog App/main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/97b20ea528d6223a4817").json()
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+if __name__ == "__main__":
+    app.run(debug=True)
