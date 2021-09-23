449ac405ab540be582b71fc29477162522d0fdf2
Prachet Shah <75517374+PrachetShah@users.noreply.github.com>
2021-09-23 19:26:16 +0530

    Add files via upload

---
+++ Day 81 Text to Morse Code Converter/main.py
@@ -0,0 +1,33 @@
+morse_convert = { 
+                    'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-',
+                }
+
+#Menu
+print('Welcome to Text to Morse Code Generator')
+sentence = input('Enter your sentence to convert into morse code: ')
+sentence = sentence.upper()
+print(sentence)
+
+#Conversion
+code = ''
+for char in sentence:
+    if char == ' ':
+        code += ' '
+    else:
+        code += morse_convert[char]
+
+print(f"Your text into Morse Code is: \n{code}")
